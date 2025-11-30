ae0834f9c58f1ec6122dea9db8e245b42401f621
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-11-30 12:12:28 +0100

    data type

---
+++ 01-Fundamentos_python/02-data_types.py
@@ -0,0 +1,17 @@
+# String
+name = "Andy"
+last_name = "Gómez"
+
+# int
+number = 2000
+age = 36
+waste = 323
+
+# foats
+decimal = 20.35
+temp = 4.5
+
+# Boolean
+boolean = True #1
+boolean_false = False #0
+is_gamer = True
