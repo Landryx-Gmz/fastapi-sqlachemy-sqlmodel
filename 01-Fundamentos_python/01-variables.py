8c00237a3c0bfc8d1706de3799d8a49ff9604545
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-11-30 12:07:44 +0100

    variables

---
+++ 01-Fundamentos_python/01-variables.py
@@ -0,0 +1,17 @@
+# Variables
+
+name = "Andy"
+full_name = "Andy Gómez"
+
+_private_name = "Andres"
+
+print(id(full_name))
+print(_private_name)
+
+#Constantes
+
+PI = 3.1416
+GREET = "HOLA"
+
+print(PI)
+print(GREET)
