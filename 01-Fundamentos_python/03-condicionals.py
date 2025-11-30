d6f27a81b3c0f9246709bbdf52f4ac209f2092ee
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-11-30 12:24:10 +0100

    condicionals

---
+++ 01-Fundamentos_python/03-condicionals.py
@@ -0,0 +1,13 @@
+
+is_new_dev = False
+interest_python = True
+knows_python = True
+
+if is_new_dev:
+    print("Te recomiendo saber Python.")
+elif interest_python:
+    print("Te recomiendo un curso en udemy")
+else:
+    print("Inicia en este mundo de la programación")
+if knows_python:
+    print("Te recomiendo aprender Django.")
