390a12a12c0f35f7c11e504824d04686bb6d33a6
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-12-07 13:58:37 +0100

    tuples

---
+++ 01-Fundamentos_python/07-tuples.py
@@ -0,0 +1,11 @@
+
+my_tuple = (1,2,3,4, "hoa", True, False,2)
+
+print(my_tuple.count(2))
+print(my_tuple.index(2))
+print(my_tuple.index(2))
+
+# son inmutables
+
+week = ("lunes", "martes", "miercoles", "jueves", "viernes")
+print(week)
