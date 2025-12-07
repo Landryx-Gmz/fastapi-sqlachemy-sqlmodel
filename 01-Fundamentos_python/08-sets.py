ccd6513d09b5533793bcc92f3f0d3f7c3b39b79f
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-12-07 14:03:11 +0100

    sets

---
+++ 01-Fundamentos_python/08-sets.py
@@ -0,0 +1,9 @@
+
+my_set = {1,1,2,2,3,4,9,5,6,7,8}
+# Son colecciones desordenadas(sin indice)
+# No permite duplicados
+
+print(my_set)
+
+username = {"ricardo", "fernando", "Andy", "fernando"}
+print(username)
