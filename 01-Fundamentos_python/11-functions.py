5505917eaa364b654f5b9c73e5bc29bae8489f9a
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-12-08 20:00:43 +0100

    functions

---
+++ 01-Fundamentos_python/11-functions.py
@@ -0,0 +1,9 @@
+# Rarametro
+def hellow(greet= "Hello", name= "Invitado"):
+    print(f"{greet}, {name}")
+
+# Argumentos
+hellow("Hola", "Andy")
+hellow("Heey", "Landryx")
+hellow()
+hellow(name= "Andres", greet="Hi!")
